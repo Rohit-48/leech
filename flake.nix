57b691a27d5c49ecefd6d8601348339dc6dd0bbb
Rohit <rohitmandavkar3577@gmail.com>
2026-07-06 12:23:35 +0530

    added flake file for reproduciblity

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "leech - realtime DNS process monitor";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+
+          packages = with pkgs; [
+
+            rustc
+            cargo
+            rust-analyzer
+
+            pkg-config
+            clang
+
+            libpcap
+
+            git
+
+            just
+            bacon
+            cargo-watch
+
+          ];
+
+        };
+      });
+}
