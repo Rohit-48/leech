7dd8e7a9ccb652aabce8c10b404728b0c73430a8
Rohit <rohitmandavkar3577@gmail.com>
2026-07-29 20:57:27 +0530

    written default doc code of pcap, but nixos sys errors occuring for linkers, need to solve this or i have to find way.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use pcap::Device;
+
 fn main() {
-    
+    let mut cap = Device::lookup().unwrap().unwrap().open().unwrap();
+
+    while let Ok(packet) = cap.next_packet() {
+        println!("Recevied packet! {:?}", packet);
+    }
 }
